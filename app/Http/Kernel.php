18f707a0f3f6b12ace539ccc4d3a3803b91cb3ba
priteshparihar-dev <priteshparihar@samantechnosys.com>
2026-05-04 18:36:28 +0530

    create cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -42,6 +42,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\CorsMiddleware::class,
         ],
     ];
 
@@ -66,5 +67,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
 
         'role.api' => \App\Http\Middleware\ApiRoleMiddleware::class,
+        'cors' => \App\Http\Middleware\CorsMiddleware::class,
     ];
 }
