db909adbef11e3b67c526aedc972522bf6ee2dc3
priteshparihar-dev <priteshparihar@samantechnosys.com>
2026-05-06 12:39:20 +0530

    add logs

--- app/Http/Controllers/Api/FunnelApiController.php
+++ app/Http/Controllers/Api/FunnelApiController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Funnel;
 use App\Models\UserFunnel;
 use Illuminate\Http\Request;
-use Illuminate\Http\Facades\Log;
+use Illuminate\Support\Facades\Log;
 
 class FunnelApiController extends Controller
 {
