a31cf54e89f2615038f5fd7dea3620c0a457ed5c
priteshparihar-dev <priteshparihar@samantechnosys.com>
2026-05-11 16:15:15 +0530

    add pdf_urls in getpatientformdata

--- app/Http/Controllers/Api/ClinicalController.php
+++ app/Http/Controllers/Api/ClinicalController.php
@@ -365,6 +365,7 @@ class ClinicalController extends Controller
                     'created_at'               => $item->created_at,
                     'note_comments'            => $latestNote?->note,
                     'note_comments_update_at'  => $latestNote?->updated_at,
+                    'pdf_url'                  => $item->pdf_url ? url('/storage/form-pdfs' . $item->pdf_url) : null,
                     'decoded_json'             => $decoded,
                 ];
             });
