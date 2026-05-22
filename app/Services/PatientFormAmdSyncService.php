18e87c2135b25cc909207b5b282b624b7e48ae8a
priteshparihar-dev <priteshparihar@samantechnosys.com>
2026-05-22 18:54:27 +0530

    add normalizeUsState in syncservice

--- app/Services/PatientFormAmdSyncService.php
+++ app/Services/PatientFormAmdSyncService.php
@@ -254,7 +254,7 @@ class PatientFormAmdSyncService
             '@address2' => (string) ($data['address1'] ?? ''),
             '@address1' => (string) ($data['address2'] ?? ''),
             '@city' => strtoupper((string) ($data['city'] ?? '')),
-            '@state' => strtoupper((string) ($data['state'] ?? '')),
+            '@state' => $this->normalizeUsState((string) ($data['state'] ?? '')),
             '@zip' => (string) ($data['zip'] ?? ''),
         ];
 
@@ -281,6 +281,41 @@ class PatientFormAmdSyncService
         }
 
         return $payload;
+    }
+
+    protected function normalizeUsState(string $state): string
+    {
+        $raw = strtoupper(trim($state));
+        if ($raw === '') {
+            return '';
+        }
+
+        $map = [
+            'ALABAMA' => 'AL', 'ALASKA' => 'AK', 'ARIZONA' => 'AZ', 'ARKANSAS' => 'AR',
+            'CALIFORNIA' => 'CA', 'COLORADO' => 'CO', 'CONNECTICUT' => 'CT', 'DELAWARE' => 'DE',
+            'FLORIDA' => 'FL', 'GEORGIA' => 'GA', 'HAWAII' => 'HI', 'IDAHO' => 'ID',
+            'ILLINOIS' => 'IL', 'INDIANA' => 'IN', 'IOWA' => 'IA', 'KANSAS' => 'KS',
+            'KENTUCKY' => 'KY', 'LOUISIANA' => 'LA', 'MAINE' => 'ME', 'MARYLAND' => 'MD',
+            'MASSACHUSETTS' => 'MA', 'MICHIGAN' => 'MI', 'MINNESOTA' => 'MN', 'MISSISSIPPI' => 'MS',
+            'MISSOURI' => 'MO', 'MONTANA' => 'MT', 'NEBRASKA' => 'NE', 'NEVADA' => 'NV',
+            'NEW HAMPSHIRE' => 'NH', 'NEW JERSEY' => 'NJ', 'NEW MEXICO' => 'NM', 'NEW YORK' => 'NY',
+            'NORTH CAROLINA' => 'NC', 'NORTH DAKOTA' => 'ND', 'OHIO' => 'OH', 'OKLAHOMA' => 'OK',
+            'OREGON' => 'OR', 'PENNSYLVANIA' => 'PA', 'RHODE ISLAND' => 'RI', 'SOUTH CAROLINA' => 'SC',
+            'SOUTH DAKOTA' => 'SD', 'TENNESSEE' => 'TN', 'TEXAS' => 'TX', 'UTAH' => 'UT',
+            'VERMONT' => 'VT', 'VIRGINIA' => 'VA', 'WASHINGTON' => 'WA', 'WEST VIRGINIA' => 'WV',
+            'WISCONSIN' => 'WI', 'WYOMING' => 'WY', 'DISTRICT OF COLUMBIA' => 'DC',
+        ];
+
+        if (isset($map[$raw])) {
+            return $map[$raw];
+        }
+
+        $lettersOnly = preg_replace('/[^A-Z]/', '', $raw) ?? '';
+        if (strlen($lettersOnly) >= 2) {
+            return substr($lettersOnly, 0, 2);
+        }
+
+        return $raw;
     }
 
     protected function extractAmdFault(string $xml): ?array
