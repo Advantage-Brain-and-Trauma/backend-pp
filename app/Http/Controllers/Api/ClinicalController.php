102e3a6a7327a45700732d148efd8780f738c5e1
priteshparihar-dev <priteshparihar@samantechnosys.com>
2026-05-25 11:18:20 +0530

    add error key in getPatientSubmitedFormData

--- app/Http/Controllers/Api/ClinicalController.php
+++ app/Http/Controllers/Api/ClinicalController.php
@@ -142,6 +142,7 @@ class ClinicalController extends Controller
 
             return response()->json([
                 'success' => false,
+                'error' => $e->getMessage(),
                 'message' => 'Error fetching patient form data'
             ], 500);
         }
