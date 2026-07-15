7fee6901afc35edac594d457194050b6cc43cf6a
priteshparihar-dev <priteshparihar@samantechnosys.com>
2026-07-15 14:33:25 +0530

    update reschedule api

--- app/Http/Controllers/Api/PatientAppointmentController.php
+++ app/Http/Controllers/Api/PatientAppointmentController.php
@@ -1505,7 +1505,7 @@ class PatientAppointmentController extends Controller
             ]);
 
             $url = config('services.app_server.staging_url')
-                . '/patient-portal-physician-update-appt-schedule/' . urlencode($userName) . '/' . $caseId . '/' . $appId;
+                . '/patient-portal/physician-update-appt-schedule/' . urlencode($userName) . '/' . $caseId . '/' . $appId;
 
             $ch = curl_init($url);
             curl_setopt_array($ch, [
